c4e59cf70e33d6341d06182f8aac609a4ceb0cc3
Серьоженко Дмитро <13789697@gsuite.duit.edu.ua>
2025-11-20 16:34:58 +0200

    Add files via upload

---
+++ flyweight-pattern.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <unordered_map> //зберігання вже створених об'єктів//
+using namespace std;
+
+class Character {
+private:
+    char symbol;
+public:
+    Character(char s) : symbol(s) {}
+    void display(int position) const {
+        cout << "Character '" << symbol << "' at position " << position << endl;
+    }
+};
+
+class CharacterFactory {
+private:
+    unordered_map<char, Character*> pool;
+public:
+    Character* getCharacter(char symbol) {
+        if (pool.find(symbol) == pool.end()) {
+            pool[symbol] = new Character(symbol);
+        }
+        return pool[symbol];
+    }
+
+    ~CharacterFactory() {
+        for (auto& pair : pool) {
+            delete pair.second;
+        }
+    }
+};
+
+int main() {
+    CharacterFactory factory;
+
+    Character* a1 = factory.getCharacter('A');
+    Character* a2 = factory.getCharacter('A');
+    Character* b1 = factory.getCharacter('B');
+
+    a1->display(1);
+    a2->display(2);
+    b1->display(3);
+
+    return 0;
+}
